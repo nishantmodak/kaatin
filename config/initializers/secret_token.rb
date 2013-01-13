ff96c1799f50afad81426257f38186469bfcb2a5
nm <nm@nm-HP-Pavilion-dv6-Notebook-PC.(none)>
2013-01-14 00:04:06 +0530

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kaatin::Application.config.secret_token = 'f20116b7f02d77923aefbd9bbede83a54ff285ea388bb37e3076fab2cb98792aaedff93167d1cedc5cf8bd3c661bffa6714540550e20612d1a917b6ecd2b0b4d'
